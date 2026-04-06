064e5ec81348b0550fe1ce2363558b3c0ce1a7ea
meowveloper <meowveloper.dev@gmail.com>
2026-04-07 00:02:39 +0700

    parse builtins

--- src/lib/parse.zig
+++ src/lib/parse.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const Io = std.Io;
 const Allocator = std.mem.Allocator;
 
+pub const Builtins = enum {
+    cd
+};
+
 pub fn parse_command(gpa: Allocator, command: []const u8) !std.ArrayList([]const u8) {
     var list = std.ArrayList([]const u8).empty;
     var it = std.mem.tokenizeAny(u8, command, " \n");
@@ -12,11 +16,28 @@ pub fn parse_command(gpa: Allocator, command: []const u8) !std.ArrayList([]const
     return list;
 }
 
-test "parse_command" {
+pub fn get_builtin(cmd: []const u8) ?Builtins {
+    if(std.meta.stringToEnum(Builtins, cmd)) |bt| {
+        return bt;
+    } else return null;
+}
+
+test "parse_not_builtin_command" {
     const gpa = std.testing.allocator;
     var list = try parse_command(gpa, "ls -la");
     defer list.deinit(gpa);
     for(list.items) |item| {
         std.debug.print("{s}\n", .{item});
     }
+    try std.testing.expect(get_builtin(list.items[0]) == null);
+}
+
+test "parse_builtin_command" {
+    const gpa = std.testing.allocator;
+    var list = try parse_command(gpa, "cd ~/.config/nvim");
+    defer list.deinit(gpa);
+    for(list.items) |item| {
+        std.debug.print("{s}\n", .{item});
+    }
+    try std.testing.expect(get_builtin(list.items[0]) != null);
 }
